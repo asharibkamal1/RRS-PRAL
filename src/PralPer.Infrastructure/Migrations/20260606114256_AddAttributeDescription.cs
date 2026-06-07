2a9d518816ad50fb5729bff03e1a71bed6018e64
asharib.kamal@pral.com.pk <asharib.kamal@pral.com.pk>
2026-06-07 09:12:37 +0500

    add migration attribute

---
+++ src/PralPer.Infrastructure/Migrations/20260606114256_AddAttributeDescription.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PralPer.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAttributeDescription : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Attributes",
+                type: "nvarchar(300)",
+                maxLength: 300,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Attributes");
+        }
+    }
+}
